7e68718dc5e17a9a63c9db55b6f1e2c12dba29fe
bodefavour <bodefavour@gmail.com>
2025-02-24 13:50:59 +0100

    implement product page in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import NavBar from "../src/components/NavBar";
 import HeroSection from "../src/components/HeroSection";
-
+import ProductGrid from "../src/components/ProductGrid";
 export default function Home() {
+  const sampleProducts = [
+    { id: 1, name: "Red Gown", price: 599.99, image: "/images/red-gown.jpg" },
+    { id: 2, name: "Royal Crown", price: 299.99, image: "/images/crown.jpg" },
+    // ... add more
+  ];
+
   return (
-    <main className="relative min-h-screen overflow-x-hidden">
+    <main>
       <NavBar />
       <HeroSection backgroundImage="/images/hero-bg.jpg" brandName="SKYFLO" />
+      <ProductGrid products={sampleProducts} />
     </main>
   );
 }
