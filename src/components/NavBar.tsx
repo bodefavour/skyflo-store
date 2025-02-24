d5fcc5b88217fe077baf58e28d8e506ae752a9d3
bodefavour <bodefavour@gmail.com>
2025-02-24 15:08:47 +0100

    edit heroicon paddings

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ import { SearchIcon, ShoppingBagIcon, MenuIcon } from "@heroicons/react/outline"
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="absolute top-0 w-full flex items-center justify-end p-4 z-40">
-      <ul className="flex space-x-4">
+    <nav className="absolute top-0 w-full flex items-center justify-end p-6 z-20">
+      <ul className="flex space-x-8">
         <li>
           <button aria-label="Search">
             <SearchIcon className="w-6 h-6 text-white hover:text-gray-300 transition-colors" />
