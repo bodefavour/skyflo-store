42210281e33e2bd056c3ac38d505708bf7519480
bodefavour <bodefavour@gmail.com>
2025-02-24 13:59:44 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../src/components/NavBar";
 import HeroSection from "../src/components/HeroSection";
 import ProductGrid from "../src/components/ProductGrid";
+
 export default function Home() {
   const sampleProducts = [
     { id: 1, name: "Red Gown", price: 599.99, image: "/images/red-gown.jpg" },
