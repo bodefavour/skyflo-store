90a7d937e7bf5b8a0107704ae7bf38fd5f95eb69
bodefavour <bodefavour@gmail.com>
2025-03-08 12:32:22 +0100

    Update heroicons-react-outline.d.ts

--- src/heroicons-react-outline.d.ts
+++ src/heroicons-react-outline.d.ts
@@ -1,5 +1,6 @@
 declare module '@heroicons/react/outline' {
     import { ComponentType, SVGProps } from 'react';
+
     export const XIcon: ComponentType<SVGProps<SVGSVGElement>>;
     export const SearchIcon: ComponentType<SVGProps<SVGSVGElement>>;
     export const ShoppingBagIcon: ComponentType<SVGProps<SVGSVGElement>>;
