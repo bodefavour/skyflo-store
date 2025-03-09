603b14632461f6dea04c0ce6c3883b52288d87c7
bodefavour <bodefavour@gmail.com>
2025-03-09 17:03:12 +0100

    Update BirthdayGiftPage.tsx

--- src/pages/BirthdayGiftPage.tsx
+++ src/pages/BirthdayGiftPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import NavBar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import ProductGrid from "../components/ProductGrid";
 
@@ -10,13 +9,26 @@ const BirthdayGifts: React.FC = () => {
     { id: 3, name: "Gift Box Set", price: 299.99, image: "/images/31343C.svg" },
   ];
 
-return (
-  <main>
-    <NavBar />
-    <HeroSection backgroundImage="/images/1001631204_enhanced.jpg.avif" brandName="Birthday" />
-    <ProductGrid products={sampleProducts} />
-  </main>
-);
+  return (
+    <main>
+      {/* Customized Hero Section */}
+      <HeroSection
+        backgroundImage="/images/1001631204_enhanced.jpg.avif"
+        className="h-[70vh] flex flex-col justify-end pb-16 text-center text-white"
+      >
+        <div className="max-w-[60%] mx-auto">
+          <h2 className="text-2xl md:text-4xl font-bold">
+            Shop from our birthday catalogue and get genuine gifts
+          </h2>
+          <button className="mt-6 px-6 py-3 bg-black text-white text-lg rounded-lg hover:bg-gray-800 transition">
+            Explore Birthday Gifts
+          </button>
+        </div>
+      </HeroSection>
+
+      <ProductGrid products={sampleProducts} />
+    </main>
+  );
 };
 
 export default BirthdayGifts;
