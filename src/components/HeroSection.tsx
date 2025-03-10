0bcbb7e2ecc61272a9a8a55948b357ca2a73cd8f
bodefavour <bodefavour@gmail.com>
2025-03-10 18:25:40 +0100

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,8 +5,10 @@ interface HeroSectionProps {
   brandName?: string; 
   textSize?: string; 
   textSpacing?: string;
-  customStyle?: React.CSSProperties; children?: ReactNode;
-  className?: string;  brandNameStyle?: React.CSSProperties;
+  customStyle?: React.CSSProperties; 
+children?: ReactNode;
+  className?: string;  
+brandNameStyle?: React.CSSProperties;
 }
 
 const HeroSection: React.FC<HeroSectionProps> = ({
