65eb2fcdbc9778c0c75a68d64ed9ff33bdb723b8
bodefavour <bodefavour@gmail.com>
2025-10-03 08:00:53 +0100

    feat: add GlobeAltIcon and UserCircleIcon exports to @heroicons/react/outline module

--- src/heroicons-react-outline.d.ts
+++ src/heroicons-react-outline.d.ts
@@ -1,8 +1,14 @@
 declare module '@heroicons/react/outline' {
     import { ComponentType, SVGProps } from 'react';
 
+    // Existing exports
     export const XIcon: ComponentType<SVGProps<SVGSVGElement>>;
     export const SearchIcon: ComponentType<SVGProps<SVGSVGElement>>;
     export const ShoppingBagIcon: ComponentType<SVGProps<SVGSVGElement>>;
     export const MenuIcon: ComponentType<SVGProps<SVGSVGElement>>;
-    export const ArrowLeftIcon: ComponentType<SVGProps<SVGSVGElement>>; }
+    export const ArrowLeftIcon: ComponentType<SVGProps<SVGSVGElement>>;
+
+    // Newly added exports required by components
+    export const GlobeAltIcon: ComponentType<SVGProps<SVGSVGElement>>;
+    export const UserCircleIcon: ComponentType<SVGProps<SVGSVGElement>>;
+}
